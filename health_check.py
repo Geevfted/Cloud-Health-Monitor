643dfa51efb360be99581ae956f9d03b2b870d37
Jesse Adaramoye <jesseadaramoye19@gmail.com>
2026-04-16 16:14:25 +0100

    Initial commit: Setup health check script

---
+++ health_check.py
@@ -0,0 +1,9 @@
+# Project 3: Cloud Health Monitor
+# This script will simulate a Load Balancer health check.
+
+def check_server():
+    print("Checking server status...")
+    # We will add the logic here next!
+
+if __name__ == "__main__":
+    check_server()
