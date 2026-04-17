5571abfefdc4ddcba19f36e4236ad9b4b7224458
Jesse Adaramoye <jesseadaramoye19@gmail.com>
2026-04-17 12:24:27 +0100

    Convert health check output to JSON format

--- health_check.py
+++ health_check.py
@@ -1,9 +1,25 @@
-# Project 3: Cloud Health Monitor
-# This script will simulate a Load Balancer health check.
+import time
+import random
+import json
 
-def check_server():
-    print("Checking server status...")
-    # We will add the logic here next!
+def run_health_check():
+    # 80% success rate
+    is_healthy = random.random() > 0.2
+    status_code = 200 if is_healthy else 500
+    level = "INFO" if is_healthy else "ERROR"
+    
+    # This dictionary is what we will eventually send to CloudWatch
+    log_entry = {
+        "timestamp": time.ctime(),
+        "status": status_code,
+        "message": "System is Healthy" if is_healthy else "System is Unhealthy",
+        "level": level
+    }
+    
+    # Convert dictionary to a JSON string
+    print(json.dumps(log_entry))
 
 if __name__ == "__main__":
-    check_server()
+    for i in range(10):
+        run_health_check()
+        time.sleep(1)
