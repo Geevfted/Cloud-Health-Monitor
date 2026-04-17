057b02d160467e4e9227b5b67e8e7f6e18fb983f
Jesse Adaramoye <jesseadaramoye19@gmail.com>
2026-04-17 12:39:13 +0100

    Integrated Boto3 for LocalStack CloudWatch logging

--- health_check.py
+++ health_check.py
@@ -1,25 +1,44 @@
 import time
 import random
 import json
+import boto3
 
-def run_health_check():
-    # 80% success rate
-    is_healthy = random.random() > 0.2
-    status_code = 200 if is_healthy else 500
-    level = "INFO" if is_healthy else "ERROR"
+LOCALSTACK_URL = "http://localhost:4566"
+GROUP_NAME = "/cloud-monitor/health-checks"
+STREAM_NAME = "heartbeat-monitor"
+
+def send_log(client, message, status):
+    # AWS requires timestamp in milliseconds
+    timestamp = int(round(time.time() * 1000))
     
-    # This dictionary is what we will eventually send to CloudWatch
-    log_entry = {
-        "timestamp": time.ctime(),
-        "status": status_code,
-        "message": "System is Healthy" if is_healthy else "System is Unhealthy",
-        "level": level
+    log_event = {
+        'timestamp': timestamp,
+        'message': json.dumps({
+            "status": status,
+            "message": message,
+            "system": "Cloud-Monitor-01"
+        })
     }
     
-    # Convert dictionary to a JSON string
-    print(json.dumps(log_entry))
+    client.put_log_events(
+        logGroupName=GROUP_NAME,
+        logStreamName=STREAM_NAME,
+        logEvents=[log_event]
+    )
 
 if __name__ == "__main__":
-    for i in range(10):
-        run_health_check()
-        time.sleep(1)
+    # Initialize the bridge
+    client = boto3.client("logs", 
+                          endpoint_url=LOCALSTACK_URL, 
+                          region_name="us-east-1",
+                          aws_access_key_id="test", 
+                          aws_secret_access_key="test")
+    
+    # Run a health check and send it to the "Cloud"
+    is_healthy = random.random() > 0.2
+    msg = "System is Healthy" if is_healthy else "System is Unhealthy"
+    stat = 200 if is_healthy else 500
+    
+    print(f"Attempting to send log: {msg}...")
+    send_log(client, msg, stat)
+    print("Log sent to LocalStack CloudWatch!")
